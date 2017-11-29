773e7d3c9e5b8497011fbb38003ecc2908f4af6e
Roberto De Ioris <roberto@20tab.com>
2017-11-29 19:41:29 +0100

    Create test_coroutine.py

---
+++ Plugins/Engine2Learn/Scripts/test_coroutine.py
@@ -0,0 +1,10 @@
+import unreal_engine as ue
+import asyncio
+import ue_asyncio
+
+async def hello_postponed():
+    print('i am the coroutine at the next tick')
+
+print('scheduling coroutine at the next tick')
+asyncio.ensure_future(hello_postponed())
+print('done')
